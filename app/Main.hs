5d943a9aa81e069b9da08be5c232f9bd61cfbec8
Eldar Aliyev <eldar.aliyev8@gmail.com>
2023-12-30 22:53:00 +0100

    use IO monad

--- app/Main.hs
+++ app/Main.hs
@@ -2,13 +2,20 @@
 
 module Main where
 
+import Control.Lens
+import Data.Aeson.Lens (key, _String)
+import qualified Data.Text.IO as TIO
 import Network.Wreq
 
 url :: String
 url = "https://api.chucknorris.io/jokes/random"
 
+f :: IO ()
+f = get url >>= \r -> TIO.putStrLn $ r ^. responseBody . key "value" . _String
+
 main :: IO ()
-main = do
-  putStrLn "Hello, Haskell!"
-  response <- get url
-  print $ show response
+-- main = do
+  -- response <- get url
+  -- let joke = response ^. responseBody . key "value" . _String
+  -- TIO.putStrLn joke
+main = f
